d8efa1f06e4539d1b51b9575bd83d19d37a36c9c
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-20 16:31:56 +0500

    problem 2 arr

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-var numbers = [20, 18, 2, 40, 293];
-var sum = 0;
+//problem_one_1
+// var numbers = [20, 18, 2, 40, 293];
+// var sum = 0;
 
-for (let i = 0; i < numbers.length; i++) {
-  sum += numbers[i];
-}
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
 
-console.log(sum);
+// console.log(sum);
+
+//problem_two_2
+// var words = ["salom", "hayer", "bino", "avtomobil", "velosipedlar", "rels"];
+// var uzunSoz = 0;
+
+// for (let i = 0; i < words.length; i++) {
+//   if (words[i].length > uzunSoz) {
+//     uzunSoz = words[i].length;
+//   }
+// }
+// console.log(`Eng uzun sozning uzunligi: ${uzunSoz}`);
