51d31e25c63e6c918e83b32cbed8b254db0993d8
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-20 16:48:52 +0500

    problem 4 arr

--- index.js
+++ index.js
@@ -20,11 +20,30 @@
 // console.log(`Eng uzun sozning uzunligi: ${uzunSoz}`);
 
 //problem_three_3
-var numbers = [3, 5, 21, 8, 19];
-var sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-  sum += numbers[i];
-  var ortachaQiymat = sum / numbers.length;
-}
+// var numbers = [3, 5, 21, 8, 19];
+// var sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+//   var ortachaQiymat = sum / numbers.length;
+// }
 
-console.log(ortachaQiymat);
+// console.log(ortachaQiymat);
+
+//problem_four_4
+let towns = ["uzb", "rus", "afg", "usa", "kor", "kaz"];
+
+console.log(
+  towns.filter((value, index) => {
+    return value === "uzb";
+  })
+);
+console.log(
+  towns.filter((value, index) => {
+    return value === "usa";
+  })
+);
+console.log(
+  towns.filter((value, index) => {
+    return value === "kor";
+  })
+);
